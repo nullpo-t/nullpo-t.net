e2f4f519877dfd62cb4aa2962472a2096509ab07
ebiiim <mail@ebiiim.com>
2018-06-08 23:13:50 +0900

    travis

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,5 @@ git config user.name "Travis CI"
 
 git init
 git add .
-git commit -m "Deploy to GitHub Pages"
-# Please generate a GitHub personal access token and set it as `GH_TOKEN` in Travis CI repository settings.
+git commit -m "Auto-Deploying via Travis CI"
 git push --force --quiet "https://${GH_TOKEN}@github.com/nullpo-t/nullpo-t.github.io.git" master:gh-pages > /dev/null 2>&1
