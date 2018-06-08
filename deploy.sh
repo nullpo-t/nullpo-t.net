2f40cdb89773a879e64f0fdf97c70d8612e6d9e6
ebiiim <mail@ebiiim.com>
2018-06-08 23:26:43 +0900

    travis: push to org repo

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ git config user.name "Travis CI"
 git init
 git add .
 git commit -m "Auto-Deploying via Travis CI"
-git push --force --quiet "https://${GH_TOKEN}@github.com/nullpo-t/nullpo-t.github.io.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@github.com/nullpo-t/nullpo-t.github.io.git" master:master > /dev/null 2>&1
